fd9bc233bb17188e3ac8c7f3957f5fa6c93d5a56
Juan Manuel <juan.manuel.contractor@macmillan.com>
2025-02-12 17:45:02 -0600

    refactor: remove unused file

--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-/**
- * A type-safe version of String.prototype.startsWith
- */
-export const startsWith: <const S extends string>(self: string, check: S) => self is `${S}${string}` = String
-  .prototype.startsWith as any;
